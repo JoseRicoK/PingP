3e2104c0a1a9fac7459c4c0f793a2e5f8903e236
conrad24 <gaboruizarias@hotmail.com>
2021-12-21 14:13:12 +0100

    añadidos

--- Marcador.py
+++ Marcador.py
@@ -1,13 +1,29 @@
 partida = 0
 while partida < 6:
+    partida = partida + 1
+    player1 = 0
+    player2 = 0
 
     condition = True
     P1 = 0
     P2 = 0
+    PM = 10
+    PM = 10
     while condition:
         print('Jugador 1 vs Jugador 2')
         print(f'    {P1}          {P2}\n')
-        puntos = input('añada punto ')
+        if P1 == (PM + 1):
+            if P2 < PM:
+                print('\nHa ganado el Jugador 1\n')
+                condition = False
+                stop = input('Pulsa enter para continuar...\n')
+        if P2 == (PM + 1):
+            if P1 < PM:
+                print('\nHa ganado el Jugador 2\n')
+                condition = False
+                stop = input('Pulsa enter para continuar...\n')
+        if condition == True:
+            puntos = input('añada punto ')
         for i in '12345qwertasdfgzxcv':
             if puntos == i:
                     P1 = P1 + 1
@@ -15,7 +31,6 @@ while partida < 6:
             if puntos == j:
                     P2 = P2 + 1
 
-
-
-
-    
+        if P1 == PM:
+            if P2 == PM:
+                PM = PM + 1
