77b9223ddfe134faf78384b342ff6a17d81072bb
conrad24 <gaboruizarias@hotmail.com>
2021-12-21 13:28:58 +0100

    marcador

--- Marcador.py
+++ Marcador.py
@@ -1,1 +1,21 @@
-for i in range('12345qwertasdfgzxcv')
+partida = 0
+while partida < 6:
+
+    condition = True
+    P1 = 0
+    P2 = 0
+    while condition:
+        print('Jugador 1 vs Jugador 2')
+        print(f'    {P1}          {P2}\n')
+        puntos = input('añada punto ')
+        for i in '12345qwertasdfgzxcv':
+            if puntos == i:
+                    P1 = P1 + 1
+        for j in 'yuiophjklñbnm67890':
+            if puntos == j:
+                    P2 = P2 + 1
+
+
+
+
+    
